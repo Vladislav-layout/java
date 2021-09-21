7f742a84afc81b4bb3f39b5bc60e65aa9781b66f
Vladislav-layout <manofunny.v@gmail.com>
2021-09-21 13:28:04 +0700

    14

--- src/com/max/idea/Task14.java
+++ src/com/max/idea/Task14.java
@@ -1,13 +1,13 @@
 package com.max.idea;
-import java.util.Random;
+
 
 public class Task14 {
     public static void main(String[] args) {
        int [] array = new int[15];
         int max = 0;
         int min = 0;
-        for(int i=0; i< array.length; i++){
-            array[i] = (int)(Math.random()*(40+1)-20);
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int)(Math.random()*(40 + 1) - 20);
             if(min > array[i]){
                 min = array[i];
             }
@@ -16,9 +16,9 @@ public class Task14 {
             }
         }
         System.out.print("Массив: [");
-        for (int j=0; j<array.length; j++) {
+        for (int j = 0; j < array.length; j++) {
             System.out.print(array[j]);
-            if(j!=14){
+            if(j != 14){
                 System.out.print(";");
             }
         }
@@ -26,6 +26,9 @@ public class Task14 {
         System.out.println("Максимальный элемент: " + max + "\nМинимальный элемент :" + min);
         if (Math.abs(max) > Math.abs(min)){
             System.out.println("Наибольший по модулю: " + max + " = |" + Math.abs(max) + "|");
+        }
+        else if (Math.abs(max) == Math.abs(min)){
+            System.out.println("Равны по модулю: |" + min + "| = |" + max + "|");
         }
         else {
             System.out.println("Наибольший по модулю: " + min + " = |" + Math.abs(min)+"|");
